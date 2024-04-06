a99f16d5a49e63317a57afebcdb973e25e348ffa
JMD139 <155589107+JMD139@users.noreply.github.com>
2024-04-06 13:02:02 -0400

    Update ProjectService.java

--- java/projects/service/ProjectService.java
+++ java/projects/service/ProjectService.java
@@ -5,6 +5,7 @@ import java.util.NoSuchElementException;
 
 import projects.dao.ProjectDao;
 import projects.entity.Project;
+import projects.exception.DbException;
 
 public class ProjectService {
 	private ProjectDao projectDao = new ProjectDao();
@@ -23,4 +24,17 @@ public class ProjectService {
 				() -> new NoSuchElementException("Project with project ID=" + projectId + "does not exist"));
 	}
 
+	public void modifyProjectDetails(Project project) {
+	  if(!projectDao.modufyProjectDetails(project)) {
+		throw new DbException("Project with ID=" + project.getProjectId() + " does not exist.");  
+	  }
+		
+	}
+
+	public void deleteProject(Integer projectId) {
+	  if(!projectDao.deleteProject(projectId)) {
+		  throw new DbException("Porject with ID=" + projectId + " does not exist.");
+	  }
+		
+	}
 }
